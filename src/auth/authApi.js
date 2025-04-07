ce0e3d3c61cc9de8657f096220cfdf8b8c411eb9
Shotech <40011872+Shotech-E@users.noreply.github.com>
2025-04-07 16:52:32 +0100

    Update authApi.js

--- src/auth/authApi.js
+++ src/auth/authApi.js
@@ -4,7 +4,7 @@ import { getBaseUrl } from "../utils/baseURL";
 const authApi = createApi({
   reducerPath: "authApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: `${getBaseUrl()}/api/auth`,
+    baseURL: `${getBaseUrl()}/api/auth`,
     credentials: "include",
   }),
 
